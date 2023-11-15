bba45a40193343fd44062ff3c9cfc98e0a848003
Juicy-and <131388626+Juicy-and@users.noreply.github.com>
2023-11-15 19:57:31 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,24 +3,44 @@
 
 class TablesClient
 {
+	pqxx::connection* conn;
 public:
+	
+	void Connect() {
+		{
+			try {
+				conn = new pqxx::connection("host = 127.0.0.1 "
+					"port=5432 "
+					"dbname=CPPbd "
+					"user=postgres "
+					"password=7845");
+				std::cout << "OK!" << std::endl;
 
-	void CreateTables(pqxx::connection& conn)
+			}
+			catch (const std::exception& e)
+			{
+				std::cout << "Connected error!" << e.what() << std::endl;
+			}
+		}
+	}
+	
+	void CreateTables()
 	{
-		pqxx::transaction t(conn);
+		pqxx::transaction t(*conn);
 
 		t.exec("create table IF NOT EXISTS Client ( "
 			"id serial primary key, "
 			"name VARCHAR not null, "
 			"lastname VARCHAR not null, "
-			"email VARCHAR not null );"); 
+			"email VARCHAR not null );");
 			
 		t.commit();
 	}
 
-	void Phone(pqxx::connection& conn)
+	void Phone()
 	{
-		pqxx::transaction phones(conn);
+		
+		pqxx::transaction phones(*conn);
 		phones.exec("create table IF NOT EXISTS Phone ( "
 			"id serial primary key, "
 			"phone bigint, "
@@ -28,47 +48,65 @@ public:
 
 		phones.commit();
 	}
+
+	void AddClient()
+	{
+		
+		pqxx::transaction ac(*conn);
+		ac.exec("INSERT INTO Client(id, name, lastname, email) values (1, 'Andrey', 'Lavrentev', 'AndLav@mail.ru')");
+		ac.commit();
+	}
+
+	void AddPhone()
+	{
+		pqxx::transaction ap(*conn);
+		ap.exec("INSERT INTO Phone(id,phone,id_phone) values (1, 88005553535, 1)");
+		ap.exec("INSERT INTO Phone (id,phone,id_phone) values (2, 8999999999, 1 )");
+		ap.commit();
+	}
+
+	void UpdateClient()
+	{
+		pqxx::transaction uc(*conn);
+		uc.exec("UPDATE Client set name='ANDREY' where id = 1");
+		uc.commit();
+	}
+
+	void DeletePhone()
+	{
+		pqxx::transaction dp(*conn);
+		dp.exec("DELETE from Phone where id=1");
+		dp.exec("DELETE from Phone where id=2");
+		dp.commit();
+	}
+
+	void DeleteClient()
+	{
+		pqxx::transaction dc(*conn);
+		dc.exec("DELETE from Client where id=1");
+		dc.commit();
+	}
+
+	void SearchClient()
+	{
+		pqxx::transaction t(*conn);
+		auto result = t.exec("SELECT * FROM Client where name = 'ANDREY'");
+		t.commit();
+	}
 };
 
 int main() {
 	setlocale(LC_ALL, "Russian");
 	try {
-
-		pqxx::connection conn("host=127.0.0.1 "
-			"port=5432 "
-			"dbname=CPPbd "
-			"user=postgres "
-			"password=7845");
-
 		TablesClient tb;
-		tb.CreateTables(conn);
-		tb.Phone(conn);
-
-		pqxx::transaction t(conn);
-
-		t.exec("INSERT INTO Client(id, name, lastname, email) values (1, 'Andrey', 'Lavrentev', 'AndLav@mail.ru')");
-		
-
-		t.exec("INSERT INTO Phone(id,phone,id_phone) values (1, 88005553535, 1)");
-		
-
-		t.exec("INSERT INTO Phone (id,phone,id_phone) values (2, 8999999999, 1 )");
-		
-		t.exec("UPDATE Client set name='ANDREY' where id = 1");
-
-		auto result = t.exec("SELECT * FROM Client where name = 'ANDREY'");
-		
-		for (auto& a : result)
-		{
-			
-		}
-
-		t.exec("DELETE from Phone where id=1");
-		t.exec("DELETE from Phone where id=2");
-		t.exec("DELETE from Client where id=1");
-
-		t.commit();
-		std::cout << "OK!" << std::endl;
+		tb.Connect();
+		tb.CreateTables();
+		tb.AddClient();
+		tb.Phone();
+		tb.UpdateClient();
+		tb.SearchClient();
+		tb.DeletePhone();
+		tb.DeleteClient();
 	}
 	catch (const std::exception& e)
 	{
